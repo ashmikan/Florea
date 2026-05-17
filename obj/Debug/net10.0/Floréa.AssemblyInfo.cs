2107ed24d65b5ca0c5ead4894675e679aeabe868
ashmikan <ashmika.nathali123@gmail.com>
2026-05-17 23:11:19 +0530

    Update favicon and add new logo images

--- obj/Debug/net10.0/Floréa.AssemblyInfo.cs
+++ obj/Debug/net10.0/Floréa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Floréa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ee3c2d475f5553db50d0f2f884184af4c5bc80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b853f2eccfd1aa274f1c2f89e0015a1b8ffca2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Floréa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Floréa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
