37f528f1349476f95ca1e0307061f68b60c4ed3e
ashmikan <ashmika.nathali123@gmail.com>
2026-05-21 10:21:42 +0530

    Add compressed data file for net10.0 build

--- obj/Debug/net10.0/Floréa.AssemblyInfo.cs
+++ obj/Debug/net10.0/Floréa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Floréa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f46c01cf4ddb1a2dd2f3ed165fadecd08673cbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2caf599e66fc87274c417ef5a6448f7d6c93a2df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Floréa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Floréa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
